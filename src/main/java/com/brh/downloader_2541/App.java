c2beb64c5dad3ab19a7863f94b68a23edc726473
srhrsb <sebastian.boettcher2@srh.de>
2025-05-26 11:33:21 +0200

    Initial commit

---
+++ src/main/java/com/brh/downloader_2541/App.java
@@ -0,0 +1,43 @@
+package com.brh.downloader_2541;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 380, 200);
+        stage.setTitle("Downloader 2541");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/com/brh/downloader_2541/Controller.java
@@ -0,0 +1,25 @@
+package com.brh.downloader_2541;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    TextField url;
+
+    @FXML
+    TextField target;
+
+
+    //ToDo: 1. Implementieren Sie einen einfachen Download, unter Nutzung der Klasse Download
+    //ToDo: 2. Sorgen Sie dafür, dass die Downloads nebenläufig funktionieren
+
+    /**
+     * Download der in URL gegebenen Datei in den Ordner, der in target gegeben ist
+     */
+    @FXML
+    protected void onDownloadClick() {
+
+    }
+}
