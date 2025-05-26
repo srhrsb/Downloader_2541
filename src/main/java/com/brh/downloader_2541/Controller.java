4542ec1e64aeb00b39867663b390e20a827a5b0b
srhrsb <sebastian.boettcher2@srh.de>
2025-05-26 11:35:36 +0200

    Startversion der Aufgaben

--- src/main/java/com/brh/downloader_2541/Controller.java
+++ src/main/java/com/brh/downloader_2541/Controller.java
@@ -11,9 +11,21 @@ public class Controller {
     @FXML
     TextField target;
 
-
-    //ToDo: 1. Implementieren Sie einen einfachen Download, unter Nutzung der Klasse Download
-    //ToDo: 2. Sorgen Sie dafür, dass die Downloads nebenläufig funktionieren
+//________AUFGABEN_________________________________________________________________________________________
+//    a) Sorgen Sie dafür, dass auf Knopfdruck ein Download in den angegebenen Folder durchgeführt wird.
+//
+//    b) Implementieren Sie, dass der Zielordner via Folder-Dialog angegeben werden kann.
+//
+//    c) Bauen Sie die Klasse "Download" so um, dass Sie nebenläufig funktioniert.
+//
+//    d) Stellen Sie fest, ob es sich bei der URL um einen Ordner handelt. Wenn das der Fall ist, soll der
+//    gesamte Ordnerinhalt runtergeladen werden, sonst nur die einzelne Datei.
+//
+//    e) Fügen Sie eine Tabelle hinzu, die die URLs anzeigen soll, die gerade runtergeladen werden. In der
+//    ersten Spalte die URL, in der zweiten die bereits geladenen Bytes.
+//
+//    f) Implementieren Sie, das alle URLs beim Start des Downloads in der Tabelle angezeigt werden und die bereits geladenen Bytes während des Downloads angezeigt werden.
+//
 
     /**
      * Download der in URL gegebenen Datei in den Ordner, der in target gegeben ist
